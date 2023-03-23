7f72ed0e7686039092b8d9b97abe405194e2d634
ryan <ryanmercurio32@gmail.com>
2023-03-23 17:03:04 +0800

    lets go later

---
+++ data copy/index.js
@@ -0,0 +1,23 @@
+// const region = require("./region.json");
+// const province = require("./province.json");
+// const city = require("./city.json");
+// const barangay = require("./barangay.json");
+
+// module.export = {
+//   region,
+//   province,
+//   city,
+//   barangay,
+// };
+
+import region from "./region.json" assert { type: "json" };
+import province from "./province.json" assert { type: "json" };
+import city from "./city.json" assert { type: "json" };
+import barangay from "./region.json" assert { type: "json" };
+
+export default {
+  region,
+  province,
+  city,
+  barangay,
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+import data from "./data/index.js";
+
+const { region, province, barangay, city } = data;
+
+//console.log(JSON.stringify(region));
+
+const serializer = async () => {};
